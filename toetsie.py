0ca80d457510dc1a80605e5fe6aa709fed85b45a
basvanbeek1 <99069932@mydavinci.nl>
2021-09-27 14:06:55 +0200

    next

---
+++ toetsie.py
@@ -0,0 +1,19 @@
+#Kolonel van Geelen:
+#1. Militaire achtergrond 
+#2. specialist in wapens en tactische complotten
+#3. 
+
+
+
+
+
+print("+++++++++++++++++++++++++++++++++++++++++")
+print("+ Welkom bij de cluedo rollen zoektocht +")
+print("+++++++++++++++++++++++++++++++++++++++++")
+print("+ Wij gaan je hier wat vragen stellen   +")
+print("+ om te kijken of je geschikt bent      +")
+print("+ voor 1 van onze vele rollen.          +")
+print("+++++++++++++++++++++++++++++++++++++++++")
+
+
+
