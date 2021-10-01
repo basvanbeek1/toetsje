cb67a30786dec564ef6d1e1c151f6fd0345e4fd9
basvanbeek1 <99069932@mydavinci.nl>
2021-10-01 09:07:13 +0200

    laaste

--- toetsie.py
+++ toetsie.py
@@ -1,21 +1,3 @@
-#Kolonel van Geelen:
-#1. heb je een Militaire achtergrond 
-#2. ben je een specialist in wapens en tactische complotten
-#3. ben je een politie deskundige 
-
-#Mevrouw de Wit
-#ben je een kok
-#heb je een fors postuur  
-#ben je wit haar 
-
-#Dominee Groenewoud
-#ben je 
-#ben je kaal 
-#heb je een fors postuur 
-
-
-
-
 print("+++++++++++++++++++++++++++++++++++++++++")
 print("+ Welkom bij de cluedo rollen zoektocht +")
 print("+++++++++++++++++++++++++++++++++++++++++")
@@ -24,22 +6,19 @@ print("+ om te kijken of je geschikt bent      +")
 print("+ voor 1 van onze vele rollen.          +")
 print("+++++++++++++++++++++++++++++++++++++++++")
 
-leeftijd = input("hoe oud ben je? \n")
+leeftijd = int(input("hoe oud ben je? \n"))
 diploma = input("Ben je in bezit van een mbo 3 opleiding \n")
 ManOfVrouw= input("Ben je een man of vrouw? \n")
 
- 
-def leeftijd_diploma():
-    if leeftijd >= 18 and diploma == "ja":
-        return True 
-    else:
-        False
+leeftijd_diploma = leeftijd >= 18 and diploma == "ja"
 
 def man():
-    VanGeelen = leeftijd_diploma == True and kaal == "nee" and bril == "ja" and snor == "nee"
+    snor = input("Heb je een snor?\n")
+    bril = input("heb je een bril?\n")
+    kaal = input("ben je kaal?\n")
+    VanGeelen = kaal == "nee" and bril == "ja" and snor == "nee" and leeftijd_diploma == True
     groenewoud = leeftijd_diploma == True and kaal == "ja" and bril == "nee" and snor == "nee"
     Pimpel = leeftijd_diploma == True and kaal == "nee" and bril == "ja" and snor == "ja"
-
     if Pimpel == True:
         return print("Je mag op auditie voor de rol van Professor Pimpel")
     elif groenewoud == True:
@@ -49,16 +28,12 @@ def man():
 
 if ManOfVrouw.lower() == "m":
     man()
-    snor = input("Heb je een snor?\n")
-    bril = input("heb je een bril?\n")
-    kaal = input("ben je kaal?\n")
-    Haar = 0 
 elif ManOfVrouw.lower() == "v":
     Haar = input("heb je lang haar?\n")
     bril = input("heb je een bril\n")
     kaal = 0
     snor = 0
-    
+
  
 
 
